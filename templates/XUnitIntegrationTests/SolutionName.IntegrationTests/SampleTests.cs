5de37d111d569f7b2b6251e0efb3070921385c83
cfrenzel <camron.frenzel@bleepworks.com>
2020-01-13 11:55:11 -0500

    xunit projects

---
+++ templates/XUnitIntegrationTests/SolutionName.IntegrationTests/SampleTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Shouldly;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Linq;
+using FakeItEasy;
+using System.Data.SqlClient;
+using Xunit.Abstractions;
+
+namespace SolutionName.IntegrationTests
+{
+    using static IntegrationTestFixture;
+    
+    [Collection("Sequential")]
+    public class SampleTests : IntegrationTestBase, IClassFixture<SampleTests.Fixture>
+    {
+        private readonly Fixture _fixture;
+        private readonly ITestOutputHelper _log;
+
+        public SampleTests(Fixture fixture, ITestOutputHelper log)
+        {
+            this._fixture = fixture;
+            this._log = log;
+            //using (var scope = NewScope())
+            //{
+            //}
+        }
+
+        public class Fixture
+        {
+            public Fixture()
+            {
+            
+            }
+        }
+
+        [Fact]
+        public async Task Should_meet_requirement()
+        {
+            1.ShouldBe(2);
+        }
+      
+    }
+}
---
+++ templates/XUnitTests/SolutionName.UnitTests/SampleTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Xunit;
+using Shouldly;
+
+namespace SolutionName.UnitTests
+{
+    public class SampleTests
+    {
+     
+        public SampleTests()
+        {
+
+        }
+
+
+        [Fact]
+        public void Should_meet_requirement()
+        {
+            0.ShouldBe(1);
+        }
+
+    }
+
+}
